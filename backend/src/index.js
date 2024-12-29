331255bd80b0a32d1706851f616219d4050d4d25
Zynalex9 <alzn952@gmail.com>
2024-12-29 12:58:59 +0500

    ENV

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,13 +7,13 @@ dotenv.config({
 });
 connectDB();
 export const redis = new Redis({
-   host: "redis-16372.c83.us-east-1-2.ec2.redns.redis-cloud.com",
-   username: "default",
-   password: "pcklzlwlU8wFuvIgMaajf80CgaxpfzBU",
-   port: 16372,
+   host:process.env.REDIS_HOST,
+   username: process.env.REDIS_USERNAME,
+   password: process.env.REDIS_PASSWORD,
+   port: process.env.REDIS_PORT,
 });
-redis.on("error", () => {
-   console.log("failed to connect to redis");
+redis.on("error", (e) => {
+   console.log("failed to connect to redis",e);
 });
 redis.on("connect", () => {
    console.log("Connected to redis");
